640bedfa84ea3380f1230bb66b56a2fdcbe0b76e
luandadantas <ludanttas@gmail.com>
2020-04-17 19:50:48 -0300

    1008 - Salário

---
+++ iniciante/1008_Salario.py
@@ -0,0 +1,8 @@
+numero_func = int(input())
+horas_trab = int(input())
+valor_hora = float(input())
+
+SALARY = horas_trab * valor_hora
+
+print("NUMBER =", numero_func)
+print("SALARY = U$ {:.2f}".format(SALARY))
