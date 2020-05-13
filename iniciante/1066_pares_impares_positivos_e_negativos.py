57b233ec1dc6f35dba7a59a014b1d6a518f37d1b
luandadantas <ludanttas@gmail.com>
2020-05-13 13:27:18 -0300

    1066 - pares, impares, positivos e negativos

---
+++ iniciante/1066_pares_impares_positivos_e_negativos.py
@@ -0,0 +1,25 @@
+valor1 = int(input())
+valor2 = int(input())
+valor3 = int(input())
+valor4 = int(input())
+valor5 = int(input())
+
+positivos = 0
+negativos = 0
+pares = 0
+impares = 0
+
+for valor in [valor1, valor2, valor3, valor4, valor5]:
+    if valor > 0:
+        positivos += 1
+    if valor < 0:
+        negativos += 1
+    if valor % 2 == 0:
+        pares += 1
+    if valor % 2 == 1:
+        impares += 1
+                   
+print("{} valor(es) par(es)".format(pares))
+print("{} valor(es) impar(es)".format(impares))
+print("{} valor(es) positivo(s)".format(positivos))
+print("{} valor(es) negativo(s)".format(negativos))
