9d06544443b0950673014764eb39f9e403d69179
luandadantas <ludanttas@gmail.com>
2020-05-28 16:21:44 -0300

    1097 - sequencia IJ 3

---
+++ iniciante/1097_sequencia_IJ_3.py
@@ -0,0 +1,11 @@
+a = 7
+b = 6
+c = 5
+
+for I in range(1, 10, 2):
+    print("I={} J={}".format(I,a))
+    print("I={} J={}".format(I,b))
+    print("I={} J={}".format(I,c))
+    a += 2
+    b += 2
+    c += 2
