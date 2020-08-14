2c1c396ad8193892a15dde1a2ccddb7f1d2b8e1f
luandadantas <ludanttas@gmail.com>
2020-08-14 13:52:47 -0300

    1184 - Abaixo da diagonal principal

---
+++ iniciante/1184_abaixo_da_diagonal_principal.py
@@ -0,0 +1,20 @@
+operacao = input()
+list_matriz = []
+list_operacao = []
+
+for i in range(12):
+    list_linha = []
+    for j in range(12):
+        valor = float(input())
+        list_linha.append(valor)
+    list_matriz.append(list_linha)
+
+for a in range(12):
+    for b in range(12):
+        if b < a:
+            list_operacao.append(list_matriz[a][b])
+
+if operacao == "S":
+    print(f"{sum(list_operacao):.1f}")
+if operacao == "M":
+    print(f"{sum(list_operacao)/len(list_operacao):.1f}")
