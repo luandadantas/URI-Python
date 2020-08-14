2cb67f39725b20e6fcbbeaf27d04c4ba05dba665
luandadantas <ludanttas@gmail.com>
2020-08-14 14:59:58 -0300

    1186 - Abaixo da diagonal secundaria

---
+++ iniciante/1186_abaixo_da_diagonal_secundaria.py
@@ -0,0 +1,21 @@
+operacao = input()
+list_matriz = []
+list_operacao = []
+matriz = 12
+
+for i in range(matriz):
+    list_linha = []
+    for i in range(matriz):
+        valor = float(input())
+        list_linha.append(valor)
+    list_matriz.append(list_linha)
+
+for a in range(matriz):
+    for b in range(matriz):
+        if a + b >= matriz:
+            list_operacao.append(list_matriz[a][b])
+
+if operacao == "S":
+    print(f"{sum(list_operacao):.1f}")
+if operacao == "M":
+    print(f"{sum(list_operacao)/len(list_operacao):.1f}")
